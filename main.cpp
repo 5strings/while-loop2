f983a5c9112413d679b8c081f4b6f0f4840ce88e
Eun Jae Lee <eunjaelee@hotmail.com>
2017-11-30 15:40:44 +1100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  WhileLoop C++
+//
+//  Created by Eun Jae Lee on 30/11/17.
+//  Copyright © 2017 Eun Jae Lee. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    int x=1;
+    
+    while (x<=1000) {
+        cout<<x<<"\n";
+        x++;
+    }
+}
